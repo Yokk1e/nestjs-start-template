b3b421dd2d77900226af64030b75fd8d027167fd
satthabut <satthabut.o@gmail.com>
2021-10-11 01:15:59 +0700

    - [chore] add libs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('NEST_PORT') || 3000;
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: false,
+      whitelist: true,
+      transform: true,
+      validateCustomDecorators: true,
+    }),
+  );
+
+  if (process.env.API_DOCUMENT_ENABLE === 'TRUE') {
+    const options = new DocumentBuilder()
+      .addBearerAuth()
+      .setTitle('API')
+      .setDescription('API')
+      .setVersion('0.0.1')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api-docs', app, document);
+  }
+  await app.listen(port);
 }
 bootstrap();
