9298a475d8d36c646261e27356175548fbcef4b8
satthabut <satthabut.o@gmail.com>
2021-10-12 22:38:17 +0700

    - [feat] add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), UsersModule],
 })
 export class AppModule {}
