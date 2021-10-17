9e821c2660744d85b154234bce31bf106cf74abd
Yokk1e <satthabut.o@gmail.com>
2021-10-17 22:33:09 +0700

    - [feat] add permission gurad

---
+++ src/users/permissions/permission.enum.ts
@@ -0,0 +1,11 @@
+export enum Permission {
+  PERMISSION_VIEW = 'Permission_View',
+  ROLE_VIEW = 'Role_View',
+  ROLE_CREATE = 'Role_Create',
+  ROLE_EDIT = 'Role_Edit',
+  ROLE_DELETE = 'Role_Delete',
+  USER_VIEW = 'User_View',
+  USER_CREATE = 'User_Create',
+  USER_EDIT = ' User_Edit',
+  USER_DELETE = 'User_Delete',
+}
