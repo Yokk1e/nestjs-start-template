64ae398ccb7df4f8bbc324d14563508d82705d14
Yokk1e <satthabut.o@gmail.com>
2021-10-17 22:36:10 +0700

    - [fix] remove console log

--- src/auths/jwts/jwt.strategy.ts
+++ src/auths/jwts/jwt.strategy.ts
@@ -20,7 +20,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any): Promise<JwtUser> {
-    console.log(payload);
     return {
       userId: payload.sub,
       email: payload.email,
